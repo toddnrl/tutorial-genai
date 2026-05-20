9eedb4f8b4b689af0871e0b7f94eac7cb3f550ee
이상욱 <andrea144@naver.com>
2026-05-20 22:46:11 +0900

    MYMEMO_APP

---
+++ MEMONOTE/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect('memo.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS memos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            message TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+memos = []
+
+@app.route('/')
+def index():
+    conn = get_db()
+    memos = conn.execute('SELECT * FROM memos ORDER BY id DESC').fetchall()
+    conn.close()
+
+    return render_template('index.html', memos=memos)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    title = request.form.get('title')
+    message = request.form.get('message')
+
+    conn = get_db()
+    conn.execute(
+        'INSERT INTO memos (title, message) VALUES(?, ?)',
+        (title, message)
+    )
+    conn.commit()
+    conn.close()
+
+    return redirect('/')
+
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
